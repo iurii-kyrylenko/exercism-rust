b699c1f114d50a3b865287c1921dc5dfe85a0e51
iurii-kyrylenko <yuriproject@gmail.com>
2020-06-15 15:07:19 +0300

    [pascal-triangle] Re-index rows

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -11,7 +11,8 @@ impl PascalsTriangle {
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
         let mut data = Vec::with_capacity(self.count);
-        for n in 1..self.count + 1 {
+
+        for n in 0..self.count {
             PascalsTriangle::add_row(&mut data, n)
         }
 
@@ -19,15 +20,15 @@ impl PascalsTriangle {
     }
 
     fn add_row(data: &mut Vec<Vec<u32>>, n: usize) {
-        if n < 3 {
-            data.push(vec![1; n]);
+        if n < 2 {
+            data.push(vec![1; n + 1]);
             return;
         }
 
-        let prev_row = &data[n - 2];
-        let mut new_row = vec![1; n];
+        let prev_row = &data[n - 1];
+        let mut new_row = vec![1; n + 1];
 
-        for i in 1..n - 1 {
+        for i in 1..n {
             new_row[i] = prev_row[i - 1] + prev_row[i];
         }
 
