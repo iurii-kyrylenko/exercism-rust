d18adbd06f2d6bca2bac3301ea65cdc140d890c1
iurii-kyrylenko <yuriproject@gmail.com>
2020-06-17 23:42:43 +0300

    [etl] Implement version 2

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -12,3 +12,11 @@ pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
 
     result
 }
+
+// https://exercism.io/tracks/rust/exercises/etl/solutions/7741319fcfd040169f734d51852ffeca
+//
+pub fn transform_2(src: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    src.iter()
+        .flat_map(|(score, chars)| chars.iter().map(move |c| (c.to_ascii_lowercase(), *score)))
+        .collect()
+}
