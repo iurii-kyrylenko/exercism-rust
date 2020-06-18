d5692ff89dd28f94f892110123c9d4cdd79ccdea
iurii-kyrylenko <yuriproject@gmail.com>
2020-06-18 21:12:11 +0300

    [acronym] Implement as a state machine

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,89 @@
+enum State {
+    Space,
+    WordL,
+    WordU,
+    WordA,
+}
+
+struct Abbr<'a> {
+    current: std::str::Chars<'a>,
+    state: State,
+}
+
+impl<'a> Abbr<'a> {
+    fn new(phrase: &'a str) -> Self {
+        Abbr {
+            current: phrase.chars(),
+            state: State::Space,
+        }
+    }
+}
+
+impl Iterator for Abbr<'_> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let c = self.current.next()?;
+
+            match self.state {
+                State::Space => {
+                    if c.is_alphabetic() {
+                        self.state = if c.is_uppercase() {
+                            State::WordU
+                        } else {
+                            State::WordL
+                        };
+                        return Some(c.to_uppercase().to_string());
+                    }
+                }
+                State::WordL => {
+                    if c == '\'' {
+                        self.state = State::WordA;
+                        continue;
+                    }
+                    if !c.is_alphabetic() {
+                        self.state = State::Space;
+                        continue;
+                    }
+                    if c.is_uppercase() {
+                        self.state = State::WordU;
+                        return Some(c.to_uppercase().to_string());
+                    }
+                }
+                State::WordU => {
+                    if c == '\'' {
+                        self.state = State::WordA;
+                        continue;
+                    }
+                    if !c.is_alphabetic() {
+                        self.state = State::Space;
+                        continue;
+                    }
+                    if c.is_lowercase() {
+                        self.state = State::WordL;
+                        continue;
+                    }
+                }
+                State::WordA => {
+                    if !c.is_alphabetic() {
+                        self.state = State::Space;
+                        continue;
+                    }
+                    if c.is_alphabetic() {
+                        self.state = if c.is_uppercase() {
+                            State::WordU
+                        } else {
+                            State::WordL
+                        };
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    Abbr::new(phrase).collect()
 }
