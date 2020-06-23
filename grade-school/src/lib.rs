b70de7e00ae8d29ed3e6572a7a8593fdbf15af7a
iurii-kyrylenko <yuriproject@gmail.com>
2020-06-23 18:13:32 +0300

    [grade-school] Implement version 1

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,16 +1,21 @@
-pub struct School {}
+use std::collections::{BTreeMap, BTreeSet};
+
+pub struct School(BTreeMap<u32, BTreeSet<String>>);
 
 impl School {
-    pub fn new() -> School {
-        unimplemented!()
+    pub fn new() -> Self {
+        School(BTreeMap::new())
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
+        self.0
+            .entry(grade)
+            .or_insert(BTreeSet::new())
+            .insert(student.to_string());
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        self.0.keys().cloned().collect()
     }
 
     // If grade returned an `Option<&Vec<String>>`,
@@ -18,6 +23,6 @@ impl School {
     // By returning an owned vector instead,
     // the internal implementation is free to use whatever it chooses.
     pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
-        unimplemented!("Return the list of students in {}", grade)
+        Some(self.0.get(&grade)?.iter().map(|x| x.clone()).collect())
     }
 }
