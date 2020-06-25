ddb0e2f343baed2db0bfbb2964a2d73465e56fad
iurii-kyrylenko <yuriproject@gmail.com>
2020-06-25 16:04:03 +0300

    [queen-attack] Refactor

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -5,35 +5,28 @@ pub struct ChessPosition {
 }
 
 #[derive(Debug)]
-pub struct Queen(ChessPosition);
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        if rank < 0 || rank > 7 || file < 0 || file > 7 {
-            None
-        } else {
-            Some(Self { rank, file })
+        match (rank, file) {
+            (0..=7, 0..=7) => Some(Self { rank, file }),
+            _ => None
         }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        Self(position)
+        Self { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        let dx = (self.rank() - other.rank()).abs();
-        let dy = (self.file() - other.file()).abs();
+        let dx = self.position.rank - other.position.rank;
+        let dy = self.position.file - other.position.file;
 
-        dx == 0 || dy == 0 || dx == dy
-    }
-
-    fn rank(&self) -> i32 {
-        self.0.rank
-    }
-
-    fn file(&self) -> i32 {
-        self.0.file
+        dx == 0 || dy == 0 || dx.abs() == dy.abs()
     }
 }
