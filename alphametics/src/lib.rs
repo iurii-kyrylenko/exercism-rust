6e6ebdf354f195d9bd68c27c698d98d23815f151
iurii-kyrylenko <yuriproject@gmail.com>
2020-07-06 16:15:30 +0300

    [alphametics] Reperesent a term as indexes in permutation array

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -5,67 +5,66 @@ use std::time::Instant;
 
 struct Alphametic {
     keys: String,
-    term_map: HashMap<String, isize>,
+    term_map: HashMap<Vec<usize>, isize>,
 }
 
 impl Alphametic {
     fn new(input: &str) -> Self {
         let re = Regex::new(r"\s*(\+|==)\s*").unwrap();
         let terms: Vec<&str> = re.split(input.trim()).collect();
-        let last_term_id = terms.len() - 1;
 
-        let keys = terms.iter().map(|t| t.chars()).flatten().unique().collect();
+        let keys: String = terms.iter().map(|t| t.chars()).flatten().unique().collect();
 
-        let mut term_map: HashMap<String, isize> = HashMap::new();
+        let keys_idx_map: HashMap<char, usize> = keys.chars().zip(0..10).collect();
+
+        let mut term_map: HashMap<Vec<usize>, isize> = HashMap::new();
 
         for (i, term) in terms.iter().enumerate() {
-            let delta = if i == last_term_id { -1 } else { 1 };
-            let count = term_map.entry(term.to_string()).or_insert(0);
+            let term_as_idx = term.chars().map(|c| keys_idx_map[&c]).collect();
+            let delta = if i == terms.len() - 1 { -1 } else { 1 };
+            let count = term_map.entry(term_as_idx).or_insert(0);
             *count += delta;
         }
 
-        Self { term_map, keys }
+        Self { keys, term_map }
     }
 
-    fn char_map(&self, numbers: Vec<u8>) -> HashMap<char, u8> {
-        self.keys.chars().zip(numbers).collect()
+    fn to_number(term: &Vec<usize>, perm: &Vec<usize>) -> isize {
+        term.iter().fold(0, |acc, i| 10 * acc + perm[*i] as isize)
     }
 
-    fn to_number(term: &str, map: &HashMap<char, u8>) -> isize {
-        term.chars().fold(0, |acc, c| 10 * acc + map[&c] as isize)
-    }
-
-    fn is_lead_zero(&self, map: &HashMap<char, u8>) -> bool {
-        self.term_map
-            .keys()
-            .map(|key| key.chars().nth(0))
-            .flatten()
-            .any(|c| map[&c] == 0)
-    }
-
-    fn check(&self, map: &HashMap<char, u8>) -> bool {
+    fn check(&self, perm: &Vec<usize>) -> bool {
         let sum = self
             .term_map
             .iter()
-            .map(|(term, i)| Alphametic::to_number(term, map) * i)
+            .map(|(term, i)| Alphametic::to_number(term, perm) * i)
             .sum::<isize>();
 
-        sum == 0 && !self.is_lead_zero(map)
+        sum == 0 && !self.is_lead_zero(perm)
+    }
+
+    fn is_lead_zero(&self, perm: &Vec<usize>) -> bool {
+        self.term_map.keys().map(|key| key[0]).any(|i| perm[i] == 0)
+    }
+
+    fn key_map(&self, perm: &Vec<usize>) -> HashMap<char, u8> {
+        self.keys
+            .chars()
+            .zip(perm.iter().map(|&i| i as u8))
+            .collect()
     }
 }
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let timer = Instant::now();
     let solver = Alphametic::new(input);
-    let permutator = (0..10 as u8).permutations(solver.keys.len());
+    let permutator = (0..10).permutations(solver.keys.len());
 
     for numbers in permutator {
-        let map = solver.char_map(numbers);
-
-        match solver.check(&map) {
+        match solver.check(&numbers) {
             true => {
                 println!("========= done in {:?} ============", timer.elapsed());
-                return Some(map);
+                return Some(solver.key_map(&numbers));
             }
             _ => continue,
         }
