7a3f5267d5d27267c3ba019f61df479e74d7f4f7
iurii-kyrylenko <yuriproject@gmail.com>
2020-07-17 14:49:57 +0300

    [isbn-verifier] Implement version 1

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,23 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {:?} a valid ISBN number?", isbn);
+    let iter = isbn.chars().filter(|&c| c != '-');
+
+    if iter.clone().count() != 10 {
+        return false;
+    }
+
+    match iter.enumerate().fold(Some(0), |acc, (i, c)| {
+        let d = get_digit(i, c)?;
+        acc.map(|s| s + (10 - i as u32) * d)
+    }) {
+        None => false,
+        Some(s) => s % 11 == 0,
+    }
+}
+
+fn get_digit(pos: usize, c: char) -> Option<u32> {
+    match (pos, c) {
+        (9, 'X') => Some(10),
+        _ => c.to_digit(10),
+    }
 }
