57629cb1e18b022948c991aeac5eccec7fd72dcd
iurii-kyrylenko <yuriproject@gmail.com>
2020-07-19 13:04:49 +0300

    [pangram] Implement version 1

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,4 +1,14 @@
+use std::collections::HashSet;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    sentence
+        .chars()
+        .filter_map(|c| match c.to_ascii_uppercase() {
+            cap @ 'A'..='Z' => Some(cap),
+            _ => None,
+        })
+        .collect::<HashSet<char>>()
+        .len()
+        == 26
 }
