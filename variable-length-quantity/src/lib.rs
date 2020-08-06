5b9bb97aa4c465fd32c95b3d089f760ed4ef00bf
iurii-kyrylenko <yuriproject@gmail.com>
2020-08-06 19:19:57 +0300

    [variable-length-quantity] Handle incomplete number

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -14,18 +14,22 @@ pub fn to_bytes(values: &[u32]) -> Vec<u8> {
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
     let groups = bytes.split(|byte| byte & 0x80 == 0);
-    let ends = bytes.iter().filter(|&byte| byte & 0x80 == 0);
 
-    let result = groups
-        .zip(ends)
-        .map(bytes_to_number).collect::<Vec<_>>();
+    if let Some([]) = groups.clone().last() {
+        let ends = bytes.iter().filter(|&byte| byte & 0x80 == 0);
 
-    println!("{:?}", result);
+        let result = groups
+            .zip(ends)
+            .map(bytes_to_number)
+            .collect::<Vec<_>>();
 
-    unimplemented!("Convert the list of bytes {:?} to a list of numbers", bytes)
+        Ok(result)
+    } else {
+        Err(Error::IncompleteNumber)
+    }
 }
 
-fn number_to_bytes(value: &u32) -> Vec<u8> {
+pub fn number_to_bytes(value: &u32) -> Vec<u8> {
     if value == &0 {
         return vec![0];
     }
@@ -39,12 +43,27 @@ fn number_to_bytes(value: &u32) -> Vec<u8> {
 }
 
 fn bytes_to_number(tuple: (&[u8], &u8)) -> u32 {
+    println!("{:?}", tuple); //===========
     let result = tuple.0
         .iter()
-        .fold(0_u32, |acc, byte| (acc << 7) + (*byte & 0x7f) as u32);
+        .fold(0_u32, |acc, byte| {
+            print!("acc = {:x}, byte = {:x},", acc, byte & 0x7f);
+            let res = (acc << 7) + (*byte & 0x7f) as u32;
+            println!(" new_acc = {:x}", res);
+            res
+        });
 
+    println!("acc = {:x}, byte = {:x},", result, *tuple.1);
     (result << 7) + *tuple.1 as u32
 }
+
+// fn bytes_to_number(tuple: (&[u8], &u8)) -> Result<u32, Error> {
+//     let result = tuple.0
+//         .iter()
+//         .fold(0_u32, |acc, byte| (acc << 7) + (*byte & 0x7f) as u32);
+
+//     Ok((result << 7) + *tuple.1 as u32)
+// }
 
 fn parts(value: u32, mask: u8) -> Option<(u32, u8)> {
     if value == 0 {
