c7519f3c765a55168b609b57bee91dc40d5ef041
iurii-kyrylenko <yuriproject@gmail.com>
2020-08-16 19:16:27 +0300

    [pig-latin] Implement version 1

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,6 +1,33 @@
+#[macro_use]
+extern crate lazy_static;
+use regex::Regex;
+
+// For the sake of simplicity and according to the tests we suppose
+// that the input string has a single space as a word separator
+// and all characters are alphabetic lowercase.
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{}'",
-        input
-    );
+    input
+        .split_whitespace()
+        .map(translate_word)
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+// The 'regex' cargo doesn't support 'negative lookahead'.
+// Otherwise we could use a single reg expression to get the starting
+// consonant cluster like "^(?!xr|yt)y?((qu)?[^aeiouy]?(qu)?)*"
+fn translate_word(word: &str) -> String {
+    lazy_static! {
+        static ref RE_1: Regex = Regex::new("^(xr|yt)").unwrap();
+        static ref RE_2: Regex =
+            Regex::new("^(?P<fst>y?((qu)?[^aeiouy]?(qu)?)*)?(?P<snd>.*)?").unwrap();
+    }
+
+    let word = if RE_1.is_match(word) {
+        word.to_string()
+    } else {
+        RE_2.replace(word, "$snd$fst").to_string()
+    };
+
+    return format!("{}ay", word);
 }
--- pig-latin/tests/pig-latin.rs
+++ pig-latin/tests/pig-latin.rs
@@ -2,120 +2,140 @@ use pig_latin as pl;
 
 #[test]
 fn test_word_beginning_with_a() {
+    // #1
     assert_eq!(pl::translate("apple"), "appleay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_e() {
+    // #1
     assert_eq!(pl::translate("ear"), "earay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_i() {
+    // #1
     assert_eq!(pl::translate("igloo"), "iglooay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_o() {
+    // #1
     assert_eq!(pl::translate("object"), "objectay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_u() {
+    // #1
     assert_eq!(pl::translate("under"), "underay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_a_vowel_and_followed_by_a_qu() {
+    // #1
     assert_eq!(pl::translate("equal"), "equalay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_p() {
+    // #3
     assert_eq!(pl::translate("pig"), "igpay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_k() {
+    // #3
     assert_eq!(pl::translate("koala"), "oalakay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_y() {
+    // #2
     assert_eq!(pl::translate("yellow"), "ellowyay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_x() {
+    // #2
     assert_eq!(pl::translate("xenon"), "enonxay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_q_without_a_following_u() {
+    // #2
     assert_eq!(pl::translate("qat"), "atqay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_ch() {
+    // #2
     assert_eq!(pl::translate("chair"), "airchay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_qu() {
+    // #2
     assert_eq!(pl::translate("queen"), "eenquay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_qu_and_a_preceding_consonant() {
+    // #3
     assert_eq!(pl::translate("square"), "aresquay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_th() {
+    // #2
     assert_eq!(pl::translate("therapy"), "erapythay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_thr() {
+    // #2
     assert_eq!(pl::translate("thrush"), "ushthray");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_sch() {
+    // #2
     assert_eq!(pl::translate("school"), "oolschay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_yt() {
+    // #1
     assert_eq!(pl::translate("yttria"), "yttriaay");
 }
 
 #[test]
 #[ignore]
 fn test_word_beginning_with_xr() {
+    // #1
     assert_eq!(pl::translate("xray"), "xrayay");
 }
 
 #[test]
 #[ignore]
 fn test_y_is_treated_like_a_vowel_at_the_end_of_a_consonant_cluster() {
+    // #4, #2
     assert_eq!(pl::translate("rhythm"), "ythmrhay");
 }
 
